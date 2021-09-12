be35936c1ea23a603d3a02298168a73657f255a9
Seia-Soto <seia@outlook.kr>
2021-09-12 16:33:54 +0900

    chore: update package versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,14 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'standard-with-typescript'
+    'standard'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
-    project: './tsconfig.json'
+    sourceType: 'module'
   },
   plugins: [
     '@typescript-eslint'
-  ],
-  rules: {
-  }
+  ]
 }
